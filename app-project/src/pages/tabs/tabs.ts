fb09684d38fb33b1fe4f3c26d47209f33f8899a2
thu <mingfei.net@gmail.com>
2018-03-16 11:10:10 +0800

    Update

--- app-project/src/pages/tabs/tabs.ts
+++ app-project/src/pages/tabs/tabs.ts
@@ -1,20 +1,16 @@
 import {Component} from '@angular/core';
 
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
-import {HomePage} from '../home/home';
-
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-  tab4Root = 'TestPage';
+  homePage = 'HomePage';
+  findPage = 'FindPage';
+  catPage = 'CatPage';
+  cartPage = 'CartPage';
+  userPage = 'UserPage';
 
   constructor() {
-
   }
 }
