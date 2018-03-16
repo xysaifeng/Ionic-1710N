f93047fd45da9ad36bc260b297ba43071fbf841b
thu <mingfei.net@gmail.com>
2018-03-16 16:07:06 +0800

    Update

--- server-project/app.js
+++ server-project/app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+let pool = mysql.createPool({
+    user: 'root'
+});
 
 let app = new express();
 app.use(bodyParser.json());
 
 app.post('/signUp', (req, res) => {
     let user = req.body.user;
-    console.log(user);
-    // todo DB
+    let sql = `SELECT * 
+                FROM db.user 
+                WHERE email = ?`;
+    pool.query(sql, [user.email], (err, results) => {
+        if(err) throw err;
+        if (results.length === 1) {
+            // 邮箱存在
+            res.send({"status":"exist"});
+        }
+
+        sql = `INSERT INTO 
+                db.user(email, password) 
+                VALUE(?, ?)`;
+
+    });
+
+
+
     res.send({"status":"ok"});
 });
 
