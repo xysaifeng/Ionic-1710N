ff4d42e255e5149de7e79ba568d704353c3b2686
thu <mingfei.net@gmail.com>
2018-03-19 16:59:18 +0800

    Update

--- app-project/src/pages/home/home.ts
+++ app-project/src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the HomePage page.
@@ -15,11 +15,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
+  items = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // 类比于数据库查询第一页数据
+    for (let i = 0; i < 30; i++) {
+      this.items.push(this.items.length);
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  doInfinite(infiniteScroll): void {
+    console.log('开始');
+    // setTimeout 模拟请求的时间
+    setTimeout(() => {
+      // 类比于数据库查询下一页数据
+      for (let i = 0; i < 30; i++) {
+        this.items.push(this.items.length);
+      }
+      console.log('结束');
+      // 事件对象调用 complete 函数
+      // 告诉组件新数据加载完成
+      infiniteScroll.complete();
+    }, 3000);
   }
 
 }
